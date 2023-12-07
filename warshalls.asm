08db557534b6c0067603b9cfd0f120fe9309b785
Charlie <charliepbarth@gmail.com>
2023-12-07 17:33:05 -0500

    Seems to be working

--- warshalls.asm
+++ warshalls.asm
@@ -45,26 +45,30 @@ main:
 ########################################################################### 
 
 li $s0, 3 # row size
-li $s1, 46 # load matrix into register
+li $s1, 43 # load matrix into register
 # mov $a0, $s0 # move matrix into register
 # jal displayMatrix # jump to displayMatrix
 li $s2, 0 # k
 li $s5, 1 # mask for printing bits
 li $s6, 0 # flag representing if transitive closure has been clompleted
-li $t0, 0 # index for displayMatrix
-mul $s7, $s0, $s0 # num loops for printing matrix
+
 
 #----------------------------- Display Matrix ---------------------------------
-add $t3, $s1, $zero
+
 
 la $a0, beforeGraph
 li $v0, 4
 syscall
 
+displayMatrixSetup:
+    li $t0, 0 # index for displayMatrix
+    mul $s7, $s0, $s0 # num loops for printing matrix
+    li $s5, 1
+    add $t3, $s1, $zero
+
 displayMatrix:
     li      $s5,    1
     bge     $t0,    $s7,    endDisplayMatrix
-
 
     # if t0 % s0 == 0 then print new line
     div     $t1,    $t0,     $s0
@@ -125,22 +129,29 @@ jLoop:
     add $t3, $t3, $s4 # i * n + j
     sll $t0, $t0, $t3 # mask for graph[i][j]
 
-    mul $t3, $s3, $s0 # i * n
-    add $t3, $t3, $s2 # i * n + k
-    sll $t1, $t1, $t3 # mask for graph[i][k]
+    mul $t4, $s3, $s0 # i * n
+    add $t4, $t4, $s2 # i * n + k
+    sll $t1, $t1, $t4 # mask for graph[i][k]
 
-    mul $t3, $s2, $s0 # k * n
-    add $t3, $t3, $s4 # k * n + j
-    sll $t2, $t2, $t3 # mask for graph[j][k]
+    mul $t5, $s2, $s0 # k * n
+    add $t5, $t5, $s4 # k * n + j
+    sll $t2, $t2, $t5 # mask for graph[j][k]
 
     # t7 = graph[i][j], t8 = graph[i][k], t9 = graph[j][k]
-    and $t5, $s1, $t0 # graph[i][j]
-    and $t6, $s1, $t1 # graph[i][k]
-    and $t8, $s1, $t2 # graph[j][k]
+    and $t0, $s1, $t0 # graph[i][j]
+    and $t1, $s1, $t1 # graph[i][k]
+    and $t2, $s1, $t2 # graph[j][k]
+
+    srl $t0, $t0, $t3 # shift graph[i][j] right by k
+    srl $t1, $t1, $t4 # shift graph[i][k] right by k
+    srl $t2, $t2, $t5 # shift graph[j][k] right by j
 
     # t10 = graph[i][j] | (graph[i][k] & graph[j][k])
-    and $t9, $t6, $t8 # graph[i][k] & graph[j][k]
-    or $s1, $t5, $t9 # graph[i][j] | (graph[i][k] & graph[j][k])
+    and $t6, $t1, $t2 # graph[i][k] & graph[j][k]
+    or $t0, $t0, $t6 # graph[i][j] | (graph[i][k] & graph[j][k])
+
+    sll $t0, $t0, $t3 # shift graph[i][j] left by k
+    or $s1, $s1, $t0 # graph[i][j] = graph[i][j] | (graph[i][k] & graph[j][k])
     
 
     addi $s4, $s4, 1
@@ -158,7 +169,7 @@ endTransitiveClosure:
     syscall
     li $t0, 0
 
-    j displayMatrix
+    j displayMatrixSetup
 
 end:
     li $v0, 10
